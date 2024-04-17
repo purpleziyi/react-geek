95fb3d24329b8366f8b6ea029b6d780cfd53ae5c
ziyiz <ziyiz@ulrik.uio.no>
2024-04-17 17:38:30 +0200

    initialize echarts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,38 @@
+import * as echarts from 'echarts';
+import { useEffect } from 'react';
+
+
 const Home = () => {
-    return <div>this is Home</div>
+    useEffect(() => {
+        //make sure that dom is available before rendering fiagram
+        // 1.获取渲染图标的dom节点
+        const chartDom = document.getElementById('main');
+
+        // 2.图标初始化生成图表实例对象
+        const myChart = echarts.init(chartDom);
+
+        // 3.准备图表参数
+        const option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [120, 200, 150, 80, 70, 110, 130],
+                    type: 'bar'
+                }
+            ]
+        };
+        // 4.使用图表参数完成图表的渲染
+        option && myChart.setOption(option);
+
+
+    })
+    return <div><div id='main' style={{ width: '500px', height: '400px' }}></div></div>
 }
 
 export default Home
