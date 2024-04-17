2b2720b3255efeebcd1ea9b7df22bf0d64738f33
ziyiz <ziyiz@ulrik.uio.no>
2024-04-17 18:14:24 +0200

    encapsulation of echarts

---
+++ src/pages/Home/components/BarChart.js
@@ -0,0 +1,44 @@
+// Bar chart component
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
+// 1. 把功能代码都放到这个组件中
+// 2. 把可变的部分抽象成prop参数
+
+const BarChart = ({ title }) => {
+  const chartRef = useRef(null)
+  useEffect(() => {
+    // Make sure the DOM is available before rendering the chart.
+    // 1. 获取渲染图表的dom节点
+    const chartDom = chartRef.current
+
+    // 2. 图表初始化生成图表实例对象
+    const myChart = echarts.init(chartDom)
+
+    // 3. 准备图表参数
+    const option = {
+      title: {
+        text: title  // props参数
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    }
+    // 4. 使用图表参数完成图表的渲染
+    option && myChart.setOption(option)
+
+  }, [title])
+  return <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+}
+
+export default BarChart
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,13 @@
-import * as echarts from 'echarts';
-import { useEffect } from 'react';
-
+import BarChart from "./components/BarChart"
 
 const Home = () => {
-    useEffect(() => {
-        //make sure that dom is available before rendering fiagram
-        // 1.获取渲染图标的dom节点
-        const chartDom = document.getElementById('main');
 
-        // 2.图标初始化生成图表实例对象
-        const myChart = echarts.init(chartDom);
-
-        // 3.准备图表参数
-        const option = {
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    data: [120, 200, 150, 80, 70, 110, 130],
-                    type: 'bar'
-                }
-            ]
-        };
-        // 4.使用图表参数完成图表的渲染
-        option && myChart.setOption(option);
-
-
-    })
-    return <div><div id='main' style={{ width: '500px', height: '400px' }}></div></div>
+    return (
+        <div>
+            <BarChart title={'Satisfaction of the three major frameworks'} />
+            <BarChart title={'Usage of the three major frameworks'} />
+        </div>
+    )
 }
 
 export default Home
